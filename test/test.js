7c9ef29592c21f86b10a051b635ea1b7133ea1a0
Vladimir Starkov <iamstarkov@gmail.com>
2016-08-18 16:14:36 +0200

    initial

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import compose from '../module';
 import { curry, _ } from 'curry-this';
+const { check, property, gen } = require('testcheck');
 
 import test from 'tape-catch';
 
@@ -24,7 +25,7 @@ test('#compose', ( { deepEqual: deep, equal: eq, throws, end } ) => {
     )(2),
     add(6, sqr(add(2, 2)))
   );
-
+  // (f ∘ g)(x) = f(g(x))
   const even = x => x%2 === 0;
   const map = (arr, fn) => arr.map(fn);
   const filter = (arr, fn) => arr.filter(fn);
@@ -33,6 +34,23 @@ test('#compose', ( { deepEqual: deep, equal: eq, throws, end } ) => {
       map::curry(_, sqr),
       filter::curry(_, even),
     )([1,2,3,4,5,6,7,8]), [4, 16, 36, 64]);
+
+  // # prop based testing
+  // ## helpers
+  const id = x => x;
+  const complement = x => -x;
+  // ## properties
+  const isFn = (f, g) => typeof compose(f, g) === 'function';
+  const valid = (f, g, x) => compose(f, g)(x) === f(g(x));
+  const associative = (f, g, h, x) => compose(f, compose(g, h))(x) === compose(compose(f, g), h)(x);
+  // ## additional properties
+  const worksWithId = (f, x) => compose(f, id)(x) === f(x);
+  const doubleComplement = (x) => compose(complement, complement)(x) === x;
+
+  eq(
+    check(property([gen.fn, gen.fn], isFn)).result,
+    true
+  )
 
   end();
 });
