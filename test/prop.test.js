4bf2e47059119d7dcc5656d96ba0c253b79fdf39
Andrey Melnikov <parabellum06-94@yandex.ru>
2016-08-31 21:24:55 +0300

    feat(prop.test.js): add property test with js-verify

--- test/prop.test.js
+++ test/prop.test.js
@@ -1,20 +1,57 @@
-const { check, property, gen } = require('testcheck');
+var jsc = require("jsverify");
+var _ = require("lodash");
+var gen = jsc.generator;
+var compose = (f, g) => x => f(g(x));
+var id = x => x;
+var complement = f => x => !f(x);
+var doubleComplement = compose(complement, complement);
 
-const sum = (a, b) => a + b;
+var arbType = jsc.elements(['string', 'number', 'bool', 'json']);
 
-const zero = x => sum(x, 0) === x;
-const commutative = (x, y) => sum(x, y) === sum(y, x);
-const associative = (x, y, z) => sum(x, sum(y, z)) === sum(sum(x, y), z);
+var itIsFunction = jsc.forall(arbType, arbType, arbType, (t1, t2, t3) => {
+  return jsc.forall(jsc.fn(jsc[t1], jsc[t2]), jsc.fn(jsc[t2], jsc[t3]), (f, g) => {
+    return typeof compose(f, g) === 'function';
+  })
+})
 
-const result1 = check(
-  property([gen.int], zero)
-);
+var isValid = jsc.forall(arbType, arbType, arbType, (t1, t2, t3) => {
+  return jsc.forall(jsc.fn(jsc[t1], jsc[t2]), jsc.fn(jsc[t2], jsc[t3]), (f, g) => {
+    var composed = compose(f, g);
+    return jsc.forall(jsc[t1], x => {
+      var res = f(g(x));
+      var resComposed = composed(x);
+      return res === resComposed;
+    })
+  })
+})
 
-const result2 = check(
-  property([gen.int, gen.int], commutative)
-);
-const result3 = check(
-  property([gen.int, gen.int, gen.int], associative)
-);
+var isAssociative = jsc.forall(arbType, arbType, arbType, arbType, (t1, t2, t3, t4) => {
+  return jsc.forall(jsc.fn(jsc[t1], jsc[t2]), jsc.fn(jsc[t2], jsc[t3]), jsc.fn(jsc[t3], jsc[t4]), (f, g, h) => {
+    var composedLeft = compose(f, compose(g, h));
+    var composedRight = compose(compose(f, g), h);
+    return jsc.forall(jsc[t1], x => composedLeft(x) === composedRight(x));
+  })
+})
 
-console.log({ result1, result2, result3 });
+
+var isIdNotChangeAnything = jsc.forall(arbType, arbType, arbType, (t1, t2) => {
+  return jsc.forall(jsc.fn(jsc[t1], jsc[t2]), f => {
+    var leftWithId = compose(f, id);
+    var rigthWithId = compose(id, f);
+    return jsc.forall(jsc[t1], x => leftWithId(x) === f(x) && rigthWithId(x) === f(x))
+  })
+})
+
+var isDoubleComplementIdent = jsc.forall(jsc.fn(jsc.bool), f => {
+  var doubleComplemented = doubleComplement(f);
+  return jsc.forall(jsc.bool, x => doubleComplemented(x) === f(x))
+})
+
+jsc.check(isValid)
+jsc.check(isAssociative)
+jsc.check(itIsFunction)
+jsc.check(isIdNotChangeAnything)
+jsc.check(isDoubleComplementIdent)
+
+
+
